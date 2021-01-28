99f31ebb56edadd3f6082c455a268a654a5134b1
unknown <nellysherova@mail.ru>
2021-01-28 21:06:37 +0300

    Завершение таска

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -6,8 +6,8 @@ namespace Employeee
     {
         public string firstName;
         public string lastName;
-        Employee(){}
-        Employee(string firstName, string lastName)
+        public Employee(){}
+        public Employee(string firstName, string lastName)
         {
             this.firstName = firstName;
             this.lastName = lastName;
@@ -18,52 +18,52 @@ namespace Employeee
         public double oklad;
         public double Tax()
         {
-            return (13 * salary) / 100;
+            return (13 * oklad) / 100;
         }
         public double PensionTax()
         {
-            return (salary / 100);
+            return (oklad / 100);
         }
         public double SalaryIncludingTaxes()
         {
-            return (salary - Tax() - PensionTax());
+            return (oklad - Tax() - PensionTax());
         }
         public void getInfo()
         {
-            if (position == "программист")
+            if (position == "Программист")
             {
                 oklad = 2000;
                 if (experience > 2 && experience <= 5) oklad = 5 * oklad / 100 + oklad;
                 if (experience > 5) oklad = 10 * oklad / 100 + oklad;
-                salary = oklad - SalaryIncludingTaxes();
+                salary = SalaryIncludingTaxes();
             }
-            if (position == "учитель")
+            if (position == "Учитель")
             {
                 oklad = 1000;
                 if (experience > 2 && experience <= 5) oklad = 5 * oklad / 100 + oklad;
                 if (experience > 5) oklad = 10 * oklad / 100 + oklad;
-                salary = oklad - SalaryIncludingTaxes();
+                salary = SalaryIncludingTaxes();
             }
-            if (position == "юрист")
+            if (position == "Юрист")
             {
                 oklad = 2500;
                 if (experience > 2 && experience <= 5) oklad = 5 * oklad / 100 + oklad;
                 if (experience > 5) oklad = 10 * oklad / 100 + oklad;
-                salary = oklad - SalaryIncludingTaxes();
+                salary = SalaryIncludingTaxes();
             }
-            if (position == "мененджер")
+            if (position == "Менеджер")
             {
                 oklad = 2300;
                 if (experience > 2 && experience <= 5) oklad = 5 * oklad / 100 + oklad;
                 if (experience > 5) oklad = 10 * oklad / 100 + oklad;
-                salary = oklad - SalaryIncludingTaxes();
+                salary = SalaryIncludingTaxes();
             }
-            if (position == "директор")
+            if (position == "Директор")
             {
                 oklad = 5000;
                 if (experience > 2 && experience <= 5) oklad = 5 * oklad / 100 + oklad;
                 if (experience > 5) oklad = 10 * oklad / 100 + oklad;
-                salary = oklad - SalaryIncludingTaxes();
+                salary = SalaryIncludingTaxes();
             }
             Console.WriteLine("Ваш сотрудник: ");
             Console.WriteLine($"Имя: {firstName}");
@@ -71,22 +71,29 @@ namespace Employeee
             Console.WriteLine($"Должность: {position}");
             Console.WriteLine($"Cтаж: {experience}");
             Console.WriteLine($"Оклад: {oklad}");
-            Console.WriteLine($"Налог 13% от заработной платы: {Tax()}");
-            Console.WriteLine($"Пенсионный налог 1% от заработной платы: {PensionTax()}");
-            Console.WriteLine($"Заработная плата с учетом всех налогов: {salary}");
+            Console.WriteLine($"Налог 13% от заработной платы: {Math.Round(Tax(),3)}");
+            Console.WriteLine($"Пенсионный налог 1% от заработной платы: {Math.Round(PensionTax(), 3)}");
+            Console.WriteLine($"Заработная плата с учетом всех налогов: {Math.Round(salary, 3)}");
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.ForegrounColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Вас приветствует класс Employee");
-            Console.ForegrounColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.White;
             string[] name = {"Ник", "Лейтон", "Юми", "Коннор", "Зельда", "Алия", "Герберт", "Гунтер"};
             string[] lastName1 = {"Альто", "Секемото", "Секемото", "Грей", "Май", "Ландграаб","Ландграаб", "Гот"};
             string[] posiTion = {"Директор", "Программист", "Учитель", "Юрист", "Менеджер", "Юрист", "Менеджер", "Программист"};
-            
+            int[] experience1 = {10, 4, 25, 7, 3, 15, 12, 9};
+            for (int i = 0; i < name.Length; i++)
+            {
+                Employee emp = new Employee(name[i], lastName1[i]);
+                emp.position = posiTion[i];
+                emp.experience = experience1[i];
+                emp.getInfo();
+            }
         }
     }
 }
